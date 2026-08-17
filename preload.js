bcb50a18976d971bcc1306797db63a9d58339b82
MOKSHITH Y NANDA <codersy17mc@gmail.com>
2026-08-17 19:50:48 +0530

    feat: expose secure audio transport to renderer

--- preload.js
+++ preload.js
@@ -1,12 +1,15 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('parashakthi', {
-  start: () => ipcRenderer.invoke('pipeline:start'),
+  start: (options) => ipcRenderer.invoke('pipeline:start', options),
   stop: () => ipcRenderer.invoke('pipeline:stop'),
   status: () => ipcRenderer.invoke('pipeline:status'),
+  sendAudio: (arrayBuffer) => ipcRenderer.invoke('pipeline:audio-chunk', arrayBuffer),
   onTranscript: (handler) => ipcRenderer.on('transcript', (_event, data) => handler(data)),
+  onAnswerStart: (handler) => ipcRenderer.on('answer-start', () => handler()),
   onAnswerToken: (handler) => ipcRenderer.on('answer-token', (_event, token) => handler(token)),
   onAnswerComplete: (handler) => ipcRenderer.on('answer-complete', (_event, data) => handler(data)),
   onAudioStatus: (handler) => ipcRenderer.on('audio-status', (_event, data) => handler(data)),
+  onAsrStatus: (handler) => ipcRenderer.on('asr-status', (_event, data) => handler(data)),
   onError: (handler) => ipcRenderer.on('pipeline-error', (_event, data) => handler(data))
 });
