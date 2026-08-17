142e8eae864f7ef30035b41f2230e9c380c7061c
MOKSHITH Y NANDA <codersy17mc@gmail.com>
2026-08-17 20:12:11 +0530

    Expose answer cancellation and telemetry IPC

--- preload.js
+++ preload.js
@@ -3,7 +3,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('parashakthi', {
   start: (options) => ipcRenderer.invoke('pipeline:start', options),
   stop: () => ipcRenderer.invoke('pipeline:stop'),
+  cancelAnswer: () => ipcRenderer.invoke('pipeline:cancel-answer'),
   status: () => ipcRenderer.invoke('pipeline:status'),
+  metrics: () => ipcRenderer.invoke('pipeline:metrics'),
   sendAudio: (arrayBuffer) => ipcRenderer.invoke('pipeline:audio-chunk', arrayBuffer),
   sendScreen: (base64Jpeg) => ipcRenderer.invoke('pipeline:screen', base64Jpeg),
   captureScreenNow: () => ipcRenderer.invoke('pipeline:capture-screen'),
@@ -14,6 +16,8 @@ contextBridge.exposeInMainWorld('parashakthi', {
   onAnswerStart: (handler) => ipcRenderer.on('answer-start', (_event, data) => handler(data)),
   onAnswerToken: (handler) => ipcRenderer.on('answer-token', (_event, token) => handler(token)),
   onAnswerComplete: (handler) => ipcRenderer.on('answer-complete', (_event, data) => handler(data)),
+  onAnswerCancelled: (handler) => ipcRenderer.on('answer-cancelled', (_event, data) => handler(data)),
+  onTelemetry: (handler) => ipcRenderer.on('telemetry', (_event, data) => handler(data)),
   onAudioStatus: (handler) => ipcRenderer.on('audio-status', (_event, data) => handler(data)),
   onAsrStatus: (handler) => ipcRenderer.on('asr-status', (_event, data) => handler(data)),
   onError: (handler) => ipcRenderer.on('pipeline-error', (_event, data) => handler(data))
